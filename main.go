2761004a8c7835645baa776371f6e9f5b76de9e9
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-12-12 08:54:19 -0800

    Created simple rest api in Go

--- main.go
+++ main.go
@@ -36,6 +36,18 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return 
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 // getAlbums responds with the list of all albums as JSON
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
@@ -45,6 +57,7 @@ func main() {
 	router := gin.Default()
 	router.GET("albums", getAlbums)
 	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 
 	router.Run("localhost:8080")
 }
